549949d3ea22e1c516a5afafde8d0b690b920682
Luis Jimenez <luisjimenez2812@gmail.com>
2020-03-13 14:53:53 -0600

    Recuperacion de archivo subido.

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -16,23 +16,33 @@ $('#calcularSalario').click(function(){
   var nombre = $('#nombreEmpleado').val();
   var horas = parseInt($('#horas').val());
   var precio = parseInt($('#precio').val());
+  var impuesto = 0, salarioNeto = 0, horasExtra = 0;
 
   if(horas > 0 && precio > 0 && nombre != ''){
 
     let salario = horas * precio;
     if (horas <= 35) {
-      resolucion(nombre,salario);
+      $('#resultado').append(`
+        <div id="mensaje">
+          <p>El salario del empleado <strong>`+ nombre +`</strong> es: <strong>`+ salario +`</strong></p>
+        </div>
+        `);
+        $("#calcularSalario").css("display", "none");
     }else{
-      salario  = (horas - 35) * (precio * 1.5) + precio * 35;
+      salario =  precio * 35;
+      horasExtra = (horas - 35);
+      salarioNeto = (precio * 1.5) * horasExtra + salario;
       if (salario <= 2000) {
-        console.log("libre de impuestos");
-        resolucion(nombre,salario);
+        salarioImpuesto(nombre,salario,horasExtra,impuesto,salarioNeto);
       }else if (salario <= 2220) {
         let impuesto = (salario - 2000) * 0.2;
-        resolucion(nombre,salario);
-      }
+        salarioNeto -= impuesto;
+        salarioImpuesto(nombre,salario,horasExtra,impuesto,salarioNeto);
+      }else{
         let impuesto = (salario - 2220) * 0.3 + (220 * 0.2);
-        resolucion(nombre,salario);
+        salarioNeto -= impuesto;
+        salarioImpuesto(nombre,salario,horasExtra,impuesto,salarioNeto);
+      }
     }
   }else{
     $('#resultado').append(
@@ -47,17 +57,20 @@ $('#calcularSalario').click(function(){
   }
 })
 
-function resolucion (nombre,salario){
-  $('#resultado').append(
-    `
+function salarioImpuesto (nombre,salario,horasExtra,impuesto,salarioNeto){
+  $('#resultado').append(`
     <div id="mensaje">
-      <p>El salario del empleado <strong>"`+ nombre +`"</strong> despues de deducciónes es: <strong>`+ salario +`</strong></p>
+      <p>Información de pago:</p>
+      <ul style="color: gray ">
+        <li>Salario: `+ salario +`</li>
+        <li>Horas extra: `+ horasExtra +`</li>
+        <li>impuestos: `+impuesto+`</li>
+      </ul>
+      <hr>
+      <p>El salario del empleado <strong>`+ nombre +`</strong> despues de impuesto es: <strong>`+ salarioNeto +`</strong></p>
     </div>
-    `
-  )
-
-  $("#calcularSalario").css("display", "none");
-  console.log("El salario es: " + salario)
+    `);
+    $("#calcularSalario").css("display", "none");
 }
 
 $('#btnLimpiar').click(function(){
